14910c7833b402de67d584bd79e8e27099d0da86
prakashtaz0091 <code.ing2468@gmail.com>
2025-03-14 12:43:45 +0545

    score board

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ from sprites import BackgroundLayer, CustomSpriteGroup, Player, Obstacle
 from random import randint
 
 pygame.init()
+pygame.font.init()
+
 SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -46,10 +48,26 @@ cactus = pygame.image.load('./assets/images/cactus.png').convert_alpha()
 obstacle_sf = 0.2
 cactus = pygame.transform.scale(cactus, (cactus.get_width() * obstacle_sf, cactus.get_height() * obstacle_sf))
 obs1 = Obstacle(cactus, x=SCREEN_WIDTH, y=SCREEN_HEIGHT - cactus.get_height(), velocity=7)
-obs2 = Obstacle(cactus, x=SCREEN_WIDTH+randint(300, 500), y=SCREEN_HEIGHT - cactus.get_height(), velocity=7)
+obs2 = Obstacle(cactus, x=SCREEN_WIDTH+randint(400, 500), y=SCREEN_HEIGHT - cactus.get_height(), velocity=7)
+obs3 = Obstacle(cactus, x=obs2.rect.x+randint(400, 500), y=SCREEN_HEIGHT - cactus.get_height(), velocity=7)
 
 obstacles = pygame.sprite.Group()
-obstacles.add(obs1, obs2)
+obstacles.add(obs1, obs2, obs3)
+
+
+# Score Board
+score_box_bg = pygame.image.load('./assets/images/score_box.png').convert_alpha()
+score_box_bg_sf = 0.5
+score_box_bg = pygame.transform.scale(score_box_bg, (score_box_bg.get_width()*score_box_bg_sf, score_box_bg.get_height()*score_box_bg_sf))
+score_rect = score_box_bg.get_rect()
+score_rect.x = SCREEN_WIDTH-score_rect.width-10
+score_rect.y = 10
+score_rect_shadow = pygame.rect.Rect(score_rect.x+4, score_rect.y, score_rect.width-8, score_rect.height+4)
+score_font = pygame.font.SysFont('Arial', 30)
+score = 0
+score_text = score_font.render(f'score: {score}', True, (255, 255, 255))
+score_text_rect = score_text.get_rect()
+score_text_rect.center = (score_rect.x + score_rect.width // 2, score_rect.y + score_rect.height // 2)
 
 
 # Game Loop
@@ -75,20 +93,26 @@ while running:
             running = False
             break
     
-    
-    
-    
+
     backgrounds.update()
     backgrounds.draw(screen)
-    # bg_img_1_layer.draw(screen)
+
+
+    pygame.draw.rect(screen, (100, 100, 100), score_rect_shadow)
+    screen.blit(score_box_bg, score_rect)
+    screen.blit(score_text, score_text_rect)
+
     
     player.update()
     player.draw(screen)
     
     
     if len(obstacles) < 2:
-        new_obstacle = Obstacle(cactus, x=SCREEN_WIDTH+randint(300, 500), y=SCREEN_HEIGHT - cactus.get_height(), velocity=6)
-        obstacles.add(new_obstacle)
+        new_obstacle = Obstacle(cactus, x=SCREEN_WIDTH+randint(400, 500), y=SCREEN_HEIGHT - cactus.get_height(), velocity=7)
+        new_obstacle2 = Obstacle(cactus, x=new_obstacle.rect.x+randint(400, 600), y=SCREEN_HEIGHT - cactus.get_height(), velocity=7)
+        obstacles.add(new_obstacle, new_obstacle2)
+        score += 10
+        score_text = score_font.render(f'score: {score}', True, (255, 255, 255))
         
     obstacles.update()    
     obstacles.draw(screen)
@@ -115,11 +139,11 @@ game_over_bg_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 game_over_bg_shadow = pygame.rect.Rect(game_over_bg_rect.x+4, game_over_bg_rect.y, game_over_bg_rect.width-8, game_over_bg_rect.height+4)
 
 
-pygame.font.init()
 game_over_font = pygame.font.SysFont('Arial', 50)
 game_over_text = game_over_font.render('Game Over', True, (255, 0, 0))
 game_over_text_rect = game_over_text.get_rect()
 game_over_text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 20)
+
 
 escape_font = pygame.font.SysFont('Arial', 20)
 escape_text = escape_font.render('Press ESC to exit', True, (0, 255, 0))
